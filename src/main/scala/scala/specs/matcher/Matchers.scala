2743cd088f2bd755e86cfc4f18625fc02aca2f88
etorreborre <etorreborre@40e86570-fb37-0410-8f9f-bd8ce7e8790c>
2007-09-03 15:11:44 +0000

    first commentaries on the Matcher class Unit tests of messages for logical combinations of matchers
    git-svn-id: http://specs.googlecode.com/svn/trunk@17 40e86570-fb37-0410-8f9f-bd8ce7e8790c

--- src/main/scala/scala/specs/matcher/Matchers.scala
+++ src/main/scala/scala/specs/matcher/Matchers.scala
@@ -21,6 +21,17 @@ abstract class AbstractMatcher[T] {
   def or(m: Matcher[T]): Matcher[T]
   def not: Matcher[T]
 }
+
+/**
+ * <p>This class is the base class for asserting if a given value, of type T, is matching an expected value</p>
+ * <p>The <code>matcher</code> parameter is a function which takes a value and return a Boolean saying if the match is ok and
+ * 2 messages: 1 if the match is ok, and another one if the match is ko. Those messages should usually specify which was the 
+ * expected value and which was the actual one</p>
+ * <p> It is also possible to use the boolean logical operator on matchers: and, or, not, xor to combine matchers together. 
+ * This is the essential reason why the ok message is included in the <code>matcher</code> function. For instance, when the 
+ * <code>not</code> operator is used, the ok message is used as a ko message</p>
+ *   
+ */
 class Matcher[T](val matcher: T => (Boolean, String, String)) extends AbstractMatcher[T] {
   def apply(a: T) = matcher(a) 
   def and(m: Matcher[T]): Matcher[T] = make[T]((a: T) => {
