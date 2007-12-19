30bd6a27d9056e3894fc36275130479d6ee2189c
etorreborre <etorreborre@40e86570-fb37-0410-8f9f-bd8ce7e8790c>
2007-12-19 03:07:43 +0000

    removed sample code from extendedThrowableUnit.scala
    git-svn-id: http://specs.googlecode.com/svn/trunk@137 40e86570-fb37-0410-8f9f-bd8ce7e8790c

--- src/test/scala/org/specs/extendedThrowableUnit.scala
+++ src/test/scala/org/specs/extendedThrowableUnit.scala
@@ -8,18 +8,5 @@ object extendedThrowableUnit extends Specification {
     "provide a location method extracting the name of the file and the line from an exception" in {
       new Exception("hello").location must_== "extendedThrowableUnit.scala:9"
      }
-      
-    "accept xml" in {
-      <toto>titi</toto> mustBe <toto>titi</toto>
-
-      val n1 = <toto><foo/></toto>
-      val n2 = <toto>
-      <foo/>
-      </toto>
-     n1 must ==/(n2)
-}
-
-    
   }
-
 }
