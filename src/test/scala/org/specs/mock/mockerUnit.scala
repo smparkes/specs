e85bd8367c28b252fe6a8bb0ed61ef9ea5f67a17
etorreborre <etorreborre@40e86570-fb37-0410-8f9f-bd8ce7e8790c>
2008-01-09 15:53:01 +0000

    the tests pass properly with scala version 2.6.1
    git-svn-id: http://specs.googlecode.com/svn/trunk@205 40e86570-fb37-0410-8f9f-bd8ce7e8790c

--- src/test/scala/org/specs/mock/mockerUnit.scala
+++ src/test/scala/org/specs/mock/mockerUnit.scala
@@ -7,6 +7,7 @@ object mockerUnitRunner extends ConsoleRunner(mockerUnit)
 class mockerUnitTest extends JUnit3(mockerUnit)
 object mockerUnit extends Specification with Sugar with ProtocolTypes {
   object mocker extends Mocker
+  class MockedClass { def method = (); def a = (); def b = (); def c = () }
   "A mocker" should {
     usingBefore {() => mocker.protocol.clear}
     "create a protocol when expecting calls" in {
@@ -14,7 +15,7 @@ object mockerUnit extends Specification with Sugar with ProtocolTypes {
       protocol mustNotBe null
     }
     "add a new expected call when recording calls" in {
-      val mock = new Object { def method = mocker.record }
+      val mock = new MockedClass() { override def method = mocker.record }
       val protocol = mocker.expect { mock.method }
       
       protocol verifies(_.isSpecified)
@@ -22,20 +23,20 @@ object mockerUnit extends Specification with Sugar with ProtocolTypes {
       protocol.definition.expectedCalls must beLike { case List(ExpectedCall(_)) => ok }
     }
     "add a new received call when receiving calls" in {
-      val mock = new Object { def method = mocker.record }
+      val mock = new MockedClass() { override def method = mocker.record }
       val protocol = mocker.expect { mock.method }
       mock.method
       
       protocol.receivedCalls must beLike { case List(ReceivedCall(_)) => ok }
     }
     "have a failure when not receiving an expected call" in {
-      val mock = new Object { def method = mocker.record }
+      val mock = new MockedClass() { override def method = mocker.record }
       val protocol = mocker.expect(inAnyOrder) { mock.method }
 
       protocol.failures must beMatching("Expected in any order .*. Received none")
     }
     "include a protocol def inside a protocol def if expectations are nested" in {
-      val mock = new Object { def a = mocker.record; def b = mocker.record; def c = mocker.record }
+      val mock = new MockedClass() { override def a = mocker.record; override def b = mocker.record; override def c = mocker.record }
       val protocol = mocker.expect {
         mock.a
         mocker.expect { mock.b } 
@@ -46,7 +47,7 @@ object mockerUnit extends Specification with Sugar with ProtocolTypes {
                                                                            ExpectedCall(_))) => ok }
     }
     "accept nested protocol defs using different protocol types: anyOf 1.of{method}; 2.of {method}" in {
-      val mock = new Object { def a = mocker.record; def b = mocker.record; def c = mocker.record }
+      val mock = new MockedClass() { override def a = mocker.record; override def b = mocker.record; override def c = mocker.record }
       val protocol = mocker.expect {
         mocker.expect(oneOf) { mock.a } 
         mocker.expect(twoOf) { mock.b } 
