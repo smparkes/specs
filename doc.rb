6c3f24243c872a33cdbedf9bca75003fdbc137df
etorreborre <etorreborre@40e86570-fb37-0410-8f9f-bd8ce7e8790c>
2008-05-25 06:52:17 +0000

    removed unnecessary files added javarebel
    git-svn-id: http://specs.googlecode.com/svn/trunk@437 40e86570-fb37-0410-8f9f-bd8ce7e8790c

--- doc.rb
+++
@@ -1,8 +0,0 @@
-CLASSPATH = 'c:\projects\scala-utils\target\classes;C:\projects\scalacheck\target\classes;C:\projects\specs\target\classes;./lib/junit-3.8.1.jar'
-open("specsdoc.bat", "w") do |f|
-  result = "scaladoc -d ./api2 -linksource -doctitle Specs -windowtitle Specs -top Specs -cp " + CLASSPATH + " "
-  Dir["./src/main/scala/scala/specs/**"].each do |p|
-    result += p + "/*.scala "
-  end
-  f.puts(result)
-end
