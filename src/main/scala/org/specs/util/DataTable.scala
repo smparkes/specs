c81762c6b777f784dcde48209ad543a02d2001a3
etorreborre <etorreborre@40e86570-fb37-0410-8f9f-bd8ce7e8790c>
2008-07-26 12:50:50 +0000

    added failure messages in the datatables
    git-svn-id: http://specs.googlecode.com/svn/trunk@598 40e86570-fb37-0410-8f9f-bd8ce7e8790c

--- src/main/scala/org/specs/util/DataTable.scala
+++ src/main/scala/org/specs/util/DataTable.scala
@@ -174,12 +174,15 @@ abstract class DataRow[+T0, +T1, +T2, +T3, +T4, +T5, +T6, +T7, +T8, +T9, +T10,
   def |>[S0 >: T0, S1 >: T1, S2 >: T2, S3 >: T3, S4 >: T4, S5 >: T5, S6 >: T6, S7 >: T7, S8 >: T8, S9 >: T9, 
         S10 >: T10, S11 >: T11, S12 >: T12, S13 >: T13, S14 >: T14, 
         S15 >: T15, S16 >: T16, S17 >: T17, S18 >: T18, S19 >: T19](row: DataRow[S0, S1, S2, S3, S4, S5, S6, S7, S8, S9, S10, S11, S12, S13, S14, S15, S16, S17, S18, S19]): DataTable[S0, S1, S2, S3, S4, S5, S6, S7, S8, S9, S10, S11, S12, S13, S14, S15, S16, S17, S18, S19] = DataTable(header, List(this, row), true)
-  override def toString = {
+  def valuesList = {
     var l: List[Any] = Nil
     for (i <- new Range(0, values.productArity, 1);
          e <- values.productElement(i) if (e != None))
            l = l:::List(e)
-    l.mkString("|", "|", "|")
+    l
+  }
+  override def toString = {
+    valuesList.mkString("|", "|", "|")
   }
 }
 trait ExecutableDataTable {
@@ -208,7 +211,6 @@ trait ExecutableDataTable {
 case class DataTable[T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19](header: TableHeader, rows: List[DataRow[T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19]], var shouldExecute: Boolean) extends ExecutableDataTable { outer =>
   
   type AbstractDataRow = DataRow[T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19]
-  
   /**
    * This function can be overriden to provide another behaviour upon table failure
    */  
@@ -275,18 +277,6 @@ case class DataTable[T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13,
    * @returns a string representation of the table
    */  
   override def toString = (header.toString + "\n" + rows.mkString("\n"))
-  sealed abstract class RowResult {
-    def isOk: boolean
-  }
-  case class RowOk(row: AbstractDataRow) extends RowResult {
-    def isOk = true
-    override def toString = row.toString
-  }
-  
-  case class RowKo(row: AbstractDataRow, e: Throwable) extends RowResult {
-    def isOk = false
-    override def toString = ("x" + row.toString + " " + e.getMessage)
-  }
   /**
    * execute a row by checking the execution of the user-supplied function and
    * either displaying the row or displaying the row and an error message
@@ -297,7 +287,7 @@ case class DataTable[T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13,
     catch {
       case e: Throwable => result = RowKo(row, e)
     }
-    rowResults = rowResults:::List(result)
+    rowResults = rowResults ::: List(result)
   }
   /**
    * apply a function of one argument to the table and set the table for execution
@@ -491,3 +481,16 @@ case class DataRow20[T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13,
  * Extension of a FailureException to allow a better display of this kind of failure (used in HtmlRunner)
  */
 case class DataTableFailureException[T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19](val table: DataTable[T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19]) extends FailureException(table.results)
+sealed abstract class RowResult(val row: DataRow[T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19] forSome { type T0; type T1; type  T2; type T3; type T4; type T5; type T6; type T7; type T8; type T9; type T10; type T11; type T12; type T13; type T14; type T15; type T16; type T17; type T18; type T19 }) { 
+  def isOk: boolean
+}
+case class RowOk(override val row: DataRow[T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19] forSome { type T0; type T1; type  T2; type T3; type T4; type T5; type T6; type T7; type T8; type T9; type T10; type T11; type T12; type T13; type T14; type T15; type T16; type T17; type T18; type T19 }) extends RowResult(row) {
+  def isOk = true
+  override def toString = row.toString
+}
+  
+case class RowKo(override val row: DataRow[T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19] forSome { type T0; type T1; type  T2; type T3; type T4; type T5; type T6; type T7; type T8; type T9; type T10; type T11; type T12; type T13; type T14; type T15; type T16; type T17; type T18; type T19 }, 
+                 e: Throwable) extends RowResult(row) {
+  def isOk = false
+  override def toString = ("x" + row.toString + " " + e.getMessage)
+}
--- src/test/scala/org/specs/runner/htmlRunnerUnit.scala
+++ src/test/scala/org/specs/runner/htmlRunnerUnit.scala
@@ -10,14 +10,22 @@ object htmlRunnerUnit extends Specification with DataTables {
                 1    !  1   ! 2        |
                 3    !  1   ! 5        | { (a: Int, b: Int, c: Int) => 
                     a + b must_== c  }
+  def xml = { try { table.execute } catch { case _ => } ; hRunner.xmlFor(table) }
   "the xmlFor function" should { 
     "create an html table for a DataTable" in {
-      try { table.execute } catch { case _ => }
-      hRunner.xmlFor(table) must \\(<table class="nested"></table>) 
+      xml must \\(<table class="nested"></table>) 
     }
     "create a header for the DataTable" in {
-      try { table.execute } catch { case _ => }
-      hRunner.xmlFor(table) must (\\(<td>a</td>) and \\(<td>b</td>) and \\(<td>result</td>))
+      xml  must (\\(<td>a</td>) and \\(<td>b</td>) and \\(<td>result</td>))
+    }
+    "create a row for each result" in {
+      xml must (\\(<td>1</td>) and \\(<td>1</td>) and \\(<td>2</td>))
+    }
+    "create an icon for a failure" in {
+      xml must \\(<td class="noBorder"><img src="images/icon_warning_sml.gif"/></td>)
+    }
+    "create a row for a failure message" in {
+      xml must \\(<tr><td/><td colspan="3" class="failureMessage">'4' is not equal to '5'</td></tr>)
     }
   }
 }
