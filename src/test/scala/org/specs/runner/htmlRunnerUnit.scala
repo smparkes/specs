a7667ec0f6a3d72768bbe45c298a343916bba710
etorreborre <etorreborre@40e86570-fb37-0410-8f9f-bd8ce7e8790c>
2008-07-26 11:35:05 +0000

    fixed the html runner unit test and added a nested table style
    git-svn-id: http://specs.googlecode.com/svn/trunk@597 40e86570-fb37-0410-8f9f-bd8ce7e8790c

--- src/test/scala/org/specs/runner/htmlRunnerUnit.scala
+++ src/test/scala/org/specs/runner/htmlRunnerUnit.scala
@@ -5,22 +5,21 @@ import org.specs.runner._
 import org.specs.Sugar._
 
 object htmlRunnerUnit extends Specification with DataTables {
-  lazy val table = "a"    | "b"  | "result" |
-                     1    !  1   ! 2        |
-                     1    !  1   ! 2        |
-                     3    !  1   ! 5        | { (a: Int, b: Int, c: Int) => 
+  val table = "a"    | "b"  | "result" |
+                1    !  1   ! 2        |
+                1    !  1   ! 2        |
+                3    !  1   ! 5        | { (a: Int, b: Int, c: Int) => 
                     a + b must_== c  }
-
-  "the xmlFor function" should { doBefore { executeTable }
+  "the xmlFor function" should { 
     "create an html table for a DataTable" in {
-     //  runner.xmlFor(table) must \\(<table></table>) 
+      try { table.execute } catch { case _ => }
+      hRunner.xmlFor(table) must \\(<table class="nested"></table>) 
     }
     "create a header for the DataTable" in {
-    //  runner.xmlFor(table) must (\\(<td>a</td>) and \\(<td>b</td>) and \\(<td>c</td>))
+      try { table.execute } catch { case _ => }
+      hRunner.xmlFor(table) must (\\(<td>a</td>) and \\(<td>b</td>) and \\(<td>result</td>))
     }
   }
-  def executeTable = try { table.execute } catch { case _ => }
-
 }
-object runner extends HtmlRunner(null)
+object hRunner extends HtmlRunner(null)
 class htmlRunnerUnitTest extends JUnit4(htmlRunnerUnit)
