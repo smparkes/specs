f527eb9151debe18cce24994c25d4ee132ce3134
etorreborre <etorreborre@40e86570-fb37-0410-8f9f-bd8ce7e8790c>
2009-08-28 22:58:34 +0000

    changed the strategy for integrating the conductor functionality
    git-svn-id: http://specs.googlecode.com/svn/trunk@1080 40e86570-fb37-0410-8f9f-bd8ce7e8790c

--- src/test/scala/org/specs/matcher/concurrent/threadSpec.scala
+++ src/test/scala/org/specs/matcher/concurrent/threadSpec.scala
@@ -1,23 +1,23 @@
 package org.specs.matcher.concurrent
+import org.specs._
+import org.specs.matcher._
+import java.util.concurrent.locks._
+import PimpedReadWriteLock._
 
-class threadSpec extends org.spex.Specification with org.specs.matcher.Threads {
-  val lock = new java.util.concurrent.locks.ReentrantReadWriteLock
-  import PimpedReadWriteLock._
-  
-  "test" in {
-    1 must_== 1
+class threadSpec extends SpecificationWithJUnit with Threads {
+  val lock = new ReentrantReadWriteLock
+
+  "this system" should {
+    thread("reader thread") { 
+      lock.read { 
+        waitForBeat(2) 
+      }
+    }
     thread("writer thread") {
-      waitForTick(1)
-      lock.write { tick must_== 1 }
+      waitForBeat(1)
+      lock.write { 
+        "the beat must advance" in { beat must_== 2 }
+      }
     }
   }
-  
-  
-//  thread("reader thread") { 
-//    lock.read { waitForTick(2) }
-//  }
-//  
-//  5.threads("reader thread("+name+")") {
-//    lock.read { waitForTick(2) }
-//  }
 }
